47acbabf1f2a85ced52c5b1400c8736ce83e0951
gopal <gopalpeddi379@gmail.com>
2023-07-15 13:26:32 +0530

    Listed tasks in the order of priority

--- task.js
+++ task.js
@@ -16,34 +16,59 @@ $ ./task report               # Statistics`;
 //console.log(usage);
 
 const addTask = (priority, task) => {
-    if (!task) {
-        console.log("Error: Missing tasks string. Nothing added!");
-        return;
-    } 
-    const taskLine = `${priority} ${task}`;
-    fs.appendFileSync(taskFile, taskLine + "\n");
-    console.log(`Added task: "${task}" with priority ${priority}`);
-}
+  if (!task) {
+    console.log("Error: Missing tasks string. Nothing added!");
+    return;
+  }
+  const taskLine = `${priority} ${task}`;
+  fs.appendFileSync(taskFile, taskLine + "\n");
+  console.log(`Added task: "${task}" with priority ${priority}`);
+};
+
+const listTasks = () => {
+  if (!fs.existsSync(taskFile)) {
+    console.log("There are no pending tasks!");
+    return;
+  }
+
+  const tasks = fs
+    .readFileSync(taskFile, "utf8")
+    .trim()
+    .split("\n")
+    .map((line) => line.split(" "))
+    .map(([priority, ...parts]) => ({
+      priority: parseInt(priority),
+      task: parts.join(" "),
+    }))
+    .sort((a, b) => a.priority - b.priority);
+    
+    tasks.forEach((task, idx) => {
+        const taskLine = `${idx + 1}. ${task.task} [${task.priority}]`;
+        console.log(taskLine);
+    });
+};
 
 const cli = () => {
-    const args = process.argv.slice(2);
+  const args = process.argv.slice(2);
 
-    if (args.length === 0) {
-        console.log(usage);
-        return;
-    }
-    const [cmd, ...cmdArgs] = args;
-    switch (cmd) {
-        case "add":
-            const [priority, ...parts] = cmdArgs;
-            const task = parts.join("");
-            addTask(priority, task);
-            break;
-        case "help":
-            console.log(usage);
-            break;
-    }
-
+  if (args.length === 0) {
+    console.log(usage);
+    return;
+  }
+  const [cmd, ...cmdArgs] = args;
+  switch (cmd) {
+    case "add":
+      const [priority, ...parts] = cmdArgs;
+      const task = parts.join("");
+      addTask(priority, task);
+      break;
+    case "help":
+      console.log(usage);
+      break;
+    case "ls":
+      listTasks();
+      break;
+  }
 };
 
 cli();
